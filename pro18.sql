19c125f8292ddc7387b5d1818ee032dad4f55134
unnatichauhan136568-commits <unnati.chauhan136568@marwadiuniversity.ac.in>
2026-09-17 12:32:45 +0530

    Delete pro18.sql

--- pro18.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE PROCEDURE product_update ( p_name IN VARCHAR2, p_price IN NUMBER)
- IS
-BEGIN
-    DBMS_OUTPUT.PUT_LINE('Product ' || p_name || ' updated with value: ' || p_price);
-END product_update;
-/
