50c25a05b93fd8fcc7b68bc8830e957b151efb04
unnatichauhan136568-commits <unnati.chauhan136568@marwadiuniversity.ac.in>
2026-09-21 15:22:25 +0530

    Delete calling pro/unit-4 directory

--- calling pro/unit-4/callingpro21.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE product_del (
-    p_name IN VARCHAR2
-) IS
-BEGIN
-   
-    DBMS_OUTPUT.PUT_LINE('Product ' || TRIM(p_name) || ' processing completed.');
-END product_del;
-/
